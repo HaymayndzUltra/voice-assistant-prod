48e674eab7d0bf8df615284dfb741a93e7c61518
HaymayndzUltra <althiakyler14@gmail.com>
2025-07-16 23:16:41 +0800

    PHASE1.1

--- phase1_implementation/consolidated_agents/model_manager_suite/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Model Manager Suite consolidated service.
-
-Combines GGUFModelManager, ModelManagerAgent, PredictiveLoader, and
-ModelEvaluationFramework into one FastAPI micro-service (port 7011).
-"""
--- phase1_implementation/consolidated_agents/model_manager_suite/routers/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Routers for Model Manager Suite."""
-# noqa: F401
-from . import gguf_manager_router
